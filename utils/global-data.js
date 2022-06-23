ea4571f7f871d9f46b30f06db8fdacd901b07973
anoraktrend <trente.artist@gmail.com>
2022-06-22 20:12:39 -0400

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'AnarchoGeze';
+    : 'Xeno Watcher';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : "Gezebell's Hells.Site Blog";
